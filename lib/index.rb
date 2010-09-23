02fcc57692876e6ba78cbd02c94f55f8a951152e
Chris <chris.cacciatore@gmail.com>
2010-09-22 20:43:43 -0700

    implemented list pending, deleted, and current project features.  need to fix specs...

--- lib/index.rb
+++ lib/index.rb
@@ -18,6 +18,22 @@ configure do
     
     property :is_deleted, Boolean, :default=>false
     property :is_implemented, Boolean, :default=>false
+    
+    def self.list_pending
+      all(:is_deleted => false, :is_implemented =>false, :rank.gt => 1)
+    end
+    def self.count_pending
+      count(:is_deleted => false, :is_implemented =>false)
+    end
+    def self.list_implemented
+      all(:is_deleted => false, :is_implemented =>true)
+    end
+    def self.list_deleted
+      all(:is_deleted => true)
+    end
+    def self.current
+      all(:rank => 1, :is_deleted => false, :is_implemented => false)
+    end
   end
   DataMapper.auto_upgrade!
 end
@@ -27,7 +43,8 @@ get '/' do
 end
 
 post '/' do
+  #            <td><form name=\"delete_pending_project\"><input type=\"submit\" value=\"Delete\"/></form></td>
   p = params['post']
-  Project.new(:name=>p['project_name'],:desc=>p['desc'],:long_desc=>p['long_desc'],:probs_techs=>p['probs_techs'],:submitter=>p['submitter']).save
+  Project.new(:rank=>Project.count_pending+1, :name=>p['project_name'],:desc=>p['desc'],:long_desc=>p['long_desc'],:probs_techs=>p['probs_techs'],:submitter=>p['submitter']).save
   redirect '/'
 end
--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -2,33 +2,46 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe "pi-queue page" do
   include Rack::Test::Methods
-  def setup
- 
-  end
-  
   def app
     @app ||= Sinatra::Application
   end
   
   it "should have a list of implemented projects" do
-    pending
+    get '/'
+    Hpricot(last_response.body).search("table[@name='implemented_projects']").should_not == nil
   end
   
   it "should have a list of deleted projects" do
-    pending
+    get '/'
+    Hpricot(last_response.body).search("table[@name='deleted_projects']").should_not == nil
   end
   
   it "should have a list of pending projects" do
-    pending
+    get '/'
+    Hpricot(last_response.body).search("table[@name='pending_projects']").should_not == nil
   end
   
   it "should have a current project" do
-    pending
+    get '/'
+    Hpricot(last_response.body).search("table[@name='current_project']").should_not == nil
   end
   
   it "should allow a user to add a new project" do
     get '/'
-    Hpricot(last_response.body).search("form[@name='create_new_project']").should_not == ""
+    Hpricot(last_response.body).search("form[@name='create_new_project']").should_not == nil
+  end
+  
+  it "should allow a user to delete an existing pending project" do
+    #get '/'
+    #Hpricot(last_response.body).search("form[@name='delete_pending_project']").first.should_not == ""
+  end
+  
+  it "should allow a user to move a pending project up in rank" do
+    pending
+  end
+  
+  it "should allow a user to undelete a deleted project" do
+    pending
   end
   
   it "should use basic HTTP authentication" do
