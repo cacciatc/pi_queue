8cf190d1b9c7976c6848c7840bb3df8e501e53f4
Chris <chris.cacciatore@gmail.com>
2010-09-23 21:47:28 -0700

    added http auth, fixed the deletion of projects, and implemented restore feature.

--- lib/index.rb
+++ lib/index.rb
@@ -20,30 +20,56 @@ configure do
     property :is_implemented, Boolean, :default=>false
     
     def self.list_pending
-      all(:is_deleted => false, :is_implemented =>false, :rank.gt => 1)
+      all(:is_deleted => false, :is_implemented =>false, :rank.gt => 1, :order => [ :rank.asc ])
     end
     def self.count_pending
       count(:is_deleted => false, :is_implemented =>false)
     end
     def self.list_implemented
-      all(:is_deleted => false, :is_implemented =>true)
+      all(:is_deleted => false, :is_implemented =>true, :order => [ :created_at.desc ])
     end
     def self.list_deleted
-      all(:is_deleted => true)
+      all(:is_deleted => true, :order => [ :created_at.desc ])
     end
     def self.current
       all(:rank => 1, :is_deleted => false, :is_implemented => false)
     end
+    def self.shift_rank_once_after(rank)
+      all(:rank.gt => rank).each do |p|
+        p.rank -= 1
+        p.update
+      end
+    end
   end
   DataMapper.auto_upgrade!
+end
+
+use Rack::Auth::Basic do |username, password|
+  [username, password] == ['admin', 'admin']
 end
 
 get '/' do
   erb :index
 end
 
+post '/delete/:mid' do |mid|
+  p = Project.get(mid.to_i)
+  Project.shift_rank_once_after(p.rank)
+  p.is_deleted = true
+  p.rank = -1
+  p.update
+  redirect '/'
+end
+
+post '/restore/:mid' do |mid|
+  p = Project.get(mid.to_i)
+  p.is_deleted = false
+  p.rank = Project.count_pending+1
+  p.update
+  redirect '/'
+end
+
 post '/' do
-  #            <td><form name=\"delete_pending_project\"><input type=\"submit\" value=\"Delete\"/></form></td>
   p = params['post']
   Project.new(:rank=>Project.count_pending+1, :name=>p['project_name'],:desc=>p['desc'],:long_desc=>p['long_desc'],:probs_techs=>p['probs_techs'],:submitter=>p['submitter']).save
   redirect '/'
--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -8,32 +8,32 @@ describe "pi-queue page" do
   
   it "should have a list of implemented projects" do
     get '/'
-    Hpricot(last_response.body).search("table[@name='implemented_projects']").should_not == nil
+    Hpricot(last_response.body).search("table[@name='implemented_projects']").should_not == [].empty?
   end
   
   it "should have a list of deleted projects" do
     get '/'
-    Hpricot(last_response.body).search("table[@name='deleted_projects']").should_not == nil
+    Hpricot(last_response.body).search("table[@name='deleted_projects']").should_not == [].empty?
   end
   
   it "should have a list of pending projects" do
     get '/'
-    Hpricot(last_response.body).search("table[@name='pending_projects']").should_not == nil
+    Hpricot(last_response.body).search("table[@name='pending_projects']").should_not == [].empty?
   end
   
   it "should have a current project" do
     get '/'
-    Hpricot(last_response.body).search("table[@name='current_project']").should_not == nil
+    Hpricot(last_response.body).search("table[@name='current_project']").should_not == [].empty?
   end
   
   it "should allow a user to add a new project" do
     get '/'
-    Hpricot(last_response.body).search("form[@name='create_new_project']").should_not == nil
+    Hpricot(last_response.body).search("form[@name='create_new_project']").should_not == [].empty?
   end
   
   it "should allow a user to delete an existing pending project" do
-    #get '/'
-    #Hpricot(last_response.body).search("form[@name='delete_pending_project']").first.should_not == ""
+    get '/'
+    Hpricot(last_response.body).search("form[@name=\"delete_pending_project\"]").should_not == [].empty?
   end
   
   it "should allow a user to move a pending project up in rank" do
@@ -41,10 +41,12 @@ describe "pi-queue page" do
   end
   
   it "should allow a user to undelete a deleted project" do
-    pending
+    get '/'
+    Hpricot(last_response.body).search("form[@name=\"restore_pending_project\"]").should_not == [].empty?
   end
   
   it "should use basic HTTP authentication" do
-    pending
+    get '/protected'
+    last_response.status.should == 401
   end
 end
